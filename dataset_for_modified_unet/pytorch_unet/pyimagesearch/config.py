f80c385ce4d32b87bfaeec242cd50b417b94576e
Radhika Desai <desairadhika686@gmail.com>
2024-02-27 08:38:04 +0530

    Config file updated.

--- dataset_for_modified_unet/pytorch_unet/pyimagesearch/config.py
+++ dataset_for_modified_unet/pytorch_unet/pyimagesearch/config.py
@@ -7,16 +7,19 @@ DATASET_PATH = \
     r'SkinLesionSegmentation\dataset_for_unet3\train'    
 # define the path to the images and masks dataset
 IMAGE_DATASET_PATH = \
-    "C:\\Users\\97433\\unet\\SkinLesionSegmentation\\dataset_for_unet3\\pytorch_unet\\train\\augmented_images"    
+    "dataset_for_modified_unet\pytorch_unet\dataset\test\images"    
 
 MASK_DATASET_PATH = \
-    "C:\\Users\\97433\\unet\\SkinLesionSegmentation\\dataset_for_unet3\\pytorch_unet\\train\\augmented_labels"
+    "dataset_for_modified_unet\pytorch_unet\dataset\test\labels"
+
+GRAD_CAM_IMAGE_DATASET_PATH = \
+    "dataset_for_modified_unet\pytorch_unet\dataset\test\grad_cam_images"
 
 OUTPUT_IMAGE_PATH = \
-    "C:\\Users\\97433\\unet\\SkinLesionSegmentation\\dataset_for_unet3\\pytorch_unet\\output2"
+    "dataset_for_modified\pytorch_unet\output2"
 
 MODEL_PATH = \
-    "C:\\Users\\97433\\unet\\SkinLesionSegmentation\\dataset_for_modified_unet\\pytorch_unet\\output\\unet_tgs_salt.pth"
+    "dataset_for_modified_unet\\pytorch_unet\\output2\\unet_tgs_salt.pth"
 
 # define the test split
 TEST_SPLIT = 0.15
@@ -33,8 +36,8 @@ NUM_LEVELS = 3
 # initialize learning rate, number of epochs to train for, and the
 # batch size
 INIT_LR = 0.001
-NUM_EPOCHS = 40
-BATCH_SIZE = 64
+NUM_EPOCHS = 10
+BATCH_SIZE = 1
 # define the input image dimensions
 INPUT_IMAGE_WIDTH = 150
 INPUT_IMAGE_HEIGHT = 150
@@ -45,8 +48,8 @@ BASE_OUTPUT = \
     'C:\\Users\\97433\\unet\\SkinLesionSegmentation\\dataset_for_modified_unet\\pytorch_unet\\output'
 # define the path to the output serialized model, model training
 # plot, and testing image paths
-MODEL_PATH = os.path.join(BASE_OUTPUT, "unet_tgs_salt.pth")
-PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
+#MODEL_PATH = os.path.join(BASE_OUTPUT, "unet_tgs_salt.pth")
+PLOT_PATH = "dataset_for_modified_unet\pytorch_unet\output\plot.png"
 TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
 PARALLEL_MODEL_PATH = \
     r"dataset_for_modified_unet\pytorch_unet\output\unet_tgs_salt.pth"
