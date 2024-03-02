a278c8d26f4162a53d8e32deb4de57ac30a7c39b
Radhika-Amar-Desai <120047754+Radhika-Amar-Desai@users.noreply.github.com>
2024-03-02 11:16:34 +0530

    Update modified_unet.py

--- dataset_for_modified_unet/pytorch_unet/pyimagesearch/modified_unet.py
+++ dataset_for_modified_unet/pytorch_unet/pyimagesearch/modified_unet.py
@@ -1,4 +1,4 @@
-# import the necessary packages
+#import the necessary packages
 import config
 from torch.nn import ConvTranspose2d
 from torch.nn import Conv2d
@@ -31,131 +31,171 @@ import cv2
 import torch
 import psutil
 
+print("modified_unet import successful")
 model = UNet()
 model = torch.load ( config.PARALLEL_MODEL_PATH )
 
-class Block(Module):
-	def __init__(self, inChannels, outChannels):
-		super().__init__()
-		# store the convolution and RELU layers
-		self.conv1 = Conv2d(inChannels, outChannels, 3)
-		self.relu = ReLU()
-		self.conv2 = Conv2d(outChannels, outChannels, 3)
-	def forward(self, x):
-		# apply CONV => RELU => CONV block to the inputs and return it
-		return self.conv2(self.relu(self.conv1(x)))
-	
-class Encoder(Module):
-	def __init__(self, channels=(3, 16, 32, 64)):
-		super().__init__()
-		# store the encoder blocks and maxpooling layer
-		self.encBlocks = ModuleList(
-			[Block(channels[i], channels[i + 1])
-			 	for i in range(len(channels) - 1)])
-		self.pool = MaxPool2d(2)
-	def forward(self, x):
-		# initialize an empty list to store the intermediate outputs
-		blockOutputs = []
-		# loop through the encoder blocks
-		for block in self.encBlocks:
-			# pass the inputs through the current encoder block, store
-			# the outputs, and then apply maxpooling on the output
-			x = block(x)
-			blockOutputs.append(x)
-			x = self.pool(x)
-		# return the list containing the intermediate outputs
-		return blockOutputs
+enc_output = []
+dec_output = []
 
-class Decoder(Module):
-	def __init__(self, channels=(64, 32, 16)):
-		super().__init__()
-		# initialize the number of channels, upsampler blocks, and
-		# decoder blocks
-		self.channels = channels
-		self.upconvs = ModuleList(
-			[ConvTranspose2d(channels[i], channels[i + 1], 2, 2)
-			 	for i in range(len(channels) - 1)])
-		self.dec_blocks = ModuleList(
-			[Block(int(channels[i] * 3/2), channels[i + 1])
-			 	for i in range(len(channels) - 1)])
-	def forward(self, x, grad_cam_x, encFeatures):
-		# loop through the number of channels
-		decoder_features = get_decoder_features(model, 
-										grad_cam_x)
-		for i in range(len(self.channels) - 1):
-			# pass the inputs through the upsampler blocks
-			x = self.upconvs[i](x)
-			# crop the current features from the encoder blocks,
-			# concatenate them with the current upsampled features,
-			# and pass the concatenated output through the current
-			# decoder block
-			
-			#print ( "After upconv : ", x.shape )
-			encFeat = self.crop( encFeatures[i], x )
-			x = torch.cat([x, encFeat], dim=1)
-			#print ( "Decoder-encoder  : ", x.shape )
-			grad_cam_x = decoder_features[i]
-			x = self.crop( x, grad_cam_x )
-			#print ( "x.shape" , x.shape )
-			#print ( "grad_cam_x.shape", grad_cam_x.shape )
-			x = torch.cat([x, grad_cam_x], dim=1)
-			#print (	"Decoder-another decoder : ", x.shape )
-			x = self.dec_blocks[i](x)
-			#print ( "Decoder block output : ", x.shape )
+def get_decoder_features ( unet_model , x ):
+
+    encoder = unet_model.encoder
+    decoder = unet_model.decoder
+
+    #Encoder ( including bottleneck )
+    for idx , enc_block in enumerate ( encoder.encBlocks ):
+        
+        if idx < len ( encoder.encBlocks ) - 1:
+            x = enc_block ( x )
+            x = encoder.pool ( x )
+            enc_output.append ( x )
+        
+        else :
+            x = enc_block ( x )
+            enc_output.append ( x )
+
+    #Decoder
+    enc_features = enc_output[::-1][1:]
+
+    for idx , dec_block in enumerate ( decoder.dec_blocks ):
+        #dec_input.append ( x )
+        x1 = decoder.upconvs[ idx ] ( x )
+        encFeat = decoder.crop ( enc_features[ idx ] , x1 )
+        x2 = torch.cat ( [ x1 , encFeat ] , dim = 1 )
+        x3 = dec_block ( x2 )
+        x3.detach().requires_grad = False
+        dec_output.append ( x3 )
+        x = x3
+
+    return dec_output
+
+class Block(Module):
+		def __init__(self, inChannels, outChannels):
+			super().__init__()
+			# store the convolution and RELU layers
+			self.conv1 = Conv2d(inChannels, outChannels, 3)
+			self.relu = ReLU()
+			self.conv2 = Conv2d(outChannels, outChannels, 3)
+		def forward(self, x):
+			# apply CONV => RELU => CONV block to the inputs and return it
+			return self.conv2(self.relu(self.conv1(x)))
 		
-		# return the final decoder output
-		return x
-	def crop(self, encFeatures, x):
-		# grab the dimensions of the inputs, and crop the encoder
-		# features to match the dimensions
-		# print ( "encFeatures.shape : ", encFeatures.shape )
-		# print ( "x.shape : ", x.shape )
-		(_, _, H, W) = x.shape
-		encFeatures = CenterCrop([H, W])(encFeatures)
-		# return the cropped features
-		return encFeatures
+class encoder(Module):
+		def __init__(self, channels=(3, 16, 32, 64)):
+			super().__init__()
+			#store the encoder blocks and maxpooling layer
+			self.encBlocks = ModuleList(
+				[Block(channels[i], channels[i + 1])
+					for i in range(len(channels) - 1)])
+			self.pool = MaxPool2d(2)
+		def forward(self, x):
+			#initialize an empty list to store the intermediate outputs
+			blockOutputs = []
+			#loop through the encoder blocks
+			for block in self.encBlocks:
+				#pass the inputs through the current encoder block, store
+				#the outputs, and then apply maxpooling on the output
+				x = block(x)
+				blockOutputs.append(x)
+				x = self.pool(x)
+			#return the list containing the intermediate outputs
+			return blockOutputs
+
+class decoder(Module):
+		def __init__(self, channels=(64, 32, 16)):
+			super().__init__()
+			#initialize the number of channels, upsampler blocks, and
+			#decoder blocks
+			self.channels = channels
+			self.upconvs = ModuleList(
+				[ConvTranspose2d(64, 32, 2, 2),
+				ConvTranspose2d(64, 16, 2, 2)])
+			self.dec_blocks = ModuleList(
+				[Block(channels[i] , channels[i + 1])
+					for i in range(len(channels) - 1)])
+		def forward(self, x, grad_cam_x, encFeatures):
+			#loop through the number of channels
+			decoder_features = get_decoder_features(grad_cam_x)
+			for i in range(len(self.channels) - 1):
+				#pass the inputs through the upsampler blocks
+				x = self.upconvs[i](x)
+				#crop the current features from the encoder blocks,
+				#concatenate them with the current upsampled features,
+				#and pass the concatenated output through the current
+				#decoder block
+				#print ( "After upconv : ", x.shape )
+				encFeat = self.crop( encFeatures[i], x )
+				x = torch.cat([x, encFeat], dim=1)
+				#print ( "Decoder-encoder  : ", x.shape )
+				grad_cam_x = decoder_features[i]
+				#x = self.crop( x, grad_cam_x )
+				#print ( "x.shape" , x.shape )
+				#print ( "grad_cam_x.shape", grad_cam_x.shape )
+				#x = torch.cat([x, grad_cam_x], dim=1)
+				x = self.dec_blocks[i](x)
+				#print ( "After Dec_block : ", x.shape )
+				x = self.crop( x, grad_cam_x )
+				x = torch.cat([x, grad_cam_x], dim=1)
+				#print ( "Decoder block output : ", x.shape )
+			
+			#return the final decoder output
+			return x
+		
+		def crop(self, encFeatures, x):
+			#grab the dimensions of the inputs, and crop the encoder
+			#features to match the dimensions
+			#print ( "encFeatures.shape : ", encFeatures.shape )
+			#print ( "x.shape : ", x.shape )
+			(_, _, H, W) = x.shape
+			encFeatures = CenterCrop([H, W])(encFeatures)
+			#return the cropped features
+			return encFeatures
 
 class modified_UNet(Module):
-	def __init__(self, encChannels=(3, 16, 32, 64),
-		 decChannels=(64, 32, 16),
-		 nbClasses=1, retainDim=True,
-		 outSize=(config.INPUT_IMAGE_HEIGHT,  config.INPUT_IMAGE_WIDTH)):
-		super().__init__()
-		# initialize the encoder and decoder
-		self.encoder = Encoder(encChannels)
-		self.decoder = Decoder(decChannels)
-		# initialize the regression head and store the class variables
-		self.head = Conv2d(decChannels[-1], nbClasses, 1)
-		self.retainDim = retainDim
-		self.outSize = outSize
+		def __init__(self, encChannels=(3, 16, 32, 64),
+			decChannels=(64, 32, 16),
+			nbClasses=1, retainDim=True,
+			outSize=(config.INPUT_IMAGE_HEIGHT,  config.INPUT_IMAGE_WIDTH)):
+			super().__init__()
+			#initialize the encoder and decoder
+			self.Encoder = encoder(encChannels)
+			self.Decoder = decoder(decChannels)
+			#initialize the regression head and store the class variables
+			self.head = Conv2d(decChannels[-2], nbClasses, 1)
+			self.retainDim = retainDim
+			self.outSize = outSize
 
-	def forward(self, x, grad_cam_tensor_x):
-			# grab the features from the encoder
-			encFeatures = self.encoder(x)
-			# pass the encoder features through decoder making sure that
-			# their dimensions are suited for concatenation
-			decFeatures = self.decoder(
-				x = encFeatures[::-1][0], 
-				grad_cam_x = grad_cam_tensor_x,
-				encFeatures = encFeatures[::-1][1:])
-			# pass the decoder features through the regression head to
-			# obtain the segmentation mask
-			map = self.head(decFeatures)
-			# check to see if we are retaining the original output
-			# dimensions and if so, then resize the output to match them
-			if self.retainDim:
-				map = F.interpolate(map, self.outSize)
-			# return the segmentation map
-			return map
+		def forward(self, x, grad_cam_tensor_x):
+				#grab the features from the encoder
+				encFeatures = self.Encoder(x)
+				#pass the encoder features through decoder making sure that
+				#their dimensions are suited for concatenation
+				decFeatures = self.Decoder(
+					x = encFeatures[::-1][0], 
+					grad_cam_x = grad_cam_tensor_x,
+					encFeatures = encFeatures[::-1][1:])
+				#pass the decoder features through the regression head to
+				#obtain the segmentation mask
+				map = self.head(decFeatures)
+				#check to see if we are retaining the original output
+				#dimensions and if so, then resize the output to match them
+				if self.retainDim:
+					map = F.interpolate(map, self.outSize)
+				#return the segmentation map
+				return map
 
 class CustomSegmentationDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
 
-        self.image_folder = os.path.join(root_dir, 'images')
-        self.mask_folder = os.path.join(root_dir, 'labels')
-        self.grad_cam_folder = os.path.join ( root_dir, 'grad_cam_images' )
+        self.image_folder = os.path.join(root_dir, 
+										'images')
+        self.mask_folder = os.path.join(root_dir, 
+										'labels')
+        self.grad_cam_folder = os.path.join ( root_dir, 
+											'grad_cam_images' )
 
         self.image_list = sorted(os.listdir(self.image_folder))
         self.mask_list = sorted(os.listdir(self.mask_folder))
@@ -165,7 +205,7 @@ class CustomSegmentationDataset(Dataset):
         return len(self.image_list)
 
     def __getitem__(self, idx):
-        # Load image and mask
+        #Load image and mask
         img_path = os.path.join(self.image_folder, 
                                 self.image_list[idx])
         mask_path = os.path.join(self.mask_folder, 
@@ -176,7 +216,7 @@ class CustomSegmentationDataset(Dataset):
         mask = Image.open( mask_path ).convert('L')  # Assuming masks are grayscale
         grad_cam_img = Image.open ( grad_cam_path ).convert('RGB')
         #grad_cam_img = image
-        # Apply transformations if provided
+        #Apply transformations if provided
         if self.transform:
             image = self.transform( image )
             grad_cam_img = self.transform (grad_cam_img)
@@ -207,6 +247,8 @@ testLoader = DataLoader(testDS, shuffle=True,
 		num_workers=os.cpu_count(),drop_last=True)
 
 unet = modified_UNet()
+t = torch.randn(1,3,128,128)
+unet( t , t )
 
 lossFunc = BCEWithLogitsLoss()
 opt = Adam(unet.parameters(), lr=config.INIT_LR)
@@ -248,11 +290,9 @@ if __name__ == "__main__":
             # first, zero out any previously accumulated gradients, then
             # perform backpropagation, and then update model parameters
             opt.zero_grad()
-            loss.backward(retain_graph=True)
-            #clip_grad_norm_(value_model.parameters(), clip_grad_norm)
-            # loss.backward()
+            loss.backward(retain_graph = True)
             opt.step()
-            loss.detach()
+            loss.cpu().detach()
             # add the loss to the total training loss so far
             totalTrainLoss += loss
             
