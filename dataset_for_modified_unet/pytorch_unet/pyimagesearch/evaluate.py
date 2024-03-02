845dd0eb9e442728603a1b566aaaecb63ac1c04d
Radhika-Amar-Desai <120047754+Radhika-Amar-Desai@users.noreply.github.com>
2024-03-02 11:49:01 +0530

    Update evaluate.py

--- dataset_for_modified_unet/pytorch_unet/pyimagesearch/evaluate.py
+++ dataset_for_modified_unet/pytorch_unet/pyimagesearch/evaluate.py
@@ -1,15 +1,12 @@
 import torch
-import torch.nn.functional as F
+#import torch.nn.functional as F
 from torch.utils.data import Dataset,DataLoader
 from torchvision import transforms
-
 from PIL import Image
-
 import config
-from dataset import SegmentationDataset
 import os
 from imutils import paths
-from modified_unet import modified_UNet
+from modified_unet import modified_UNet, encoder, decoder,Block
 
 class CustomSegmentationDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -18,20 +15,12 @@ class CustomSegmentationDataset(Dataset):
 
         self.image_folder = os.path.join(root_dir, 'images')
         self.mask_folder = os.path.join(root_dir, 'labels')
-        self.grad_cam_folder = os.path.join ( root_dir, 
-                                            'grad_cam_images' )
-        self.decoder_output_folder = \
-            os.path.join ( root_dir, 'decoder_tensors' )
+        self.grad_cam_folder = os.path.join ( root_dir, 'grad_cam_images' )
 
-        self.image_list = sorted(os.listdir(
-                            self.image_folder))
-        self.mask_list = sorted(os.listdir(
-                            self.mask_folder))
-        self.grad_cam_list = sorted (os.listdir (
-                            self.grad_cam_folder))
-        self.decoder_output_list = sorted ( os.listdir (
-                            self.decoder_output_folder))
-
+        self.image_list = sorted(os.listdir(self.image_folder))
+        self.mask_list = sorted(os.listdir(self.mask_folder))
+        self.grad_cam_list = sorted (os.listdir (self.grad_cam_folder))
+    
     def __len__(self):
         return len(self.image_list)
 
@@ -43,16 +32,9 @@ class CustomSegmentationDataset(Dataset):
                                 self.mask_list[idx])
         grad_cam_path = os.path.join(self.grad_cam_folder,
                                     self.grad_cam_list[idx] )
-        decoder_output_tensors_path = \
-                        os.path.join(self.decoder_output_folder,
-                                    self.decoder_output_list[idx])
-        
         image = Image.open( img_path ).convert('RGB')
         mask = Image.open( mask_path ).convert('L')  # Assuming masks are grayscale
         grad_cam_img = Image.open ( grad_cam_path ).convert('RGB')
-        decoder_output_tensors = \
-            torch.load ( decoder_output_tensors_path )
-        
         #grad_cam_img = image
         # Apply transformations if provided
         if self.transform:
@@ -60,15 +42,9 @@ class CustomSegmentationDataset(Dataset):
             grad_cam_img = self.transform (grad_cam_img)
             mask = self.transform( mask )
 
-        decoder_output_tensors[0].requires_grad = False
-        decoder_output_tensors[1].requires_grad = False
-
-        return image, grad_cam_img , mask ,\
-                decoder_output_tensors[0][0],\
-                decoder_output_tensors[1][0]
+        return image, grad_cam_img , mask
 
 if __name__ == "__main__":
-
     def calculate_iou(pred_mask, true_mask):
         intersection = torch.logical_and(pred_mask, true_mask).sum()
         union = torch.logical_or(pred_mask, true_mask).sum()
@@ -96,10 +72,10 @@ if __name__ == "__main__":
     validation_loader = DataLoader( validation_dataset , shuffle=False,
         batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
         num_workers=os.cpu_count())
-
+    print("here1")
     unet = modified_UNet()
     unet = torch.load(config.MODEL_PATH).to(config.DEVICE)
-
+    print("here2")
     unet.eval()
     total_iou = 0.0
     total_dice = 0.0
@@ -110,8 +86,7 @@ if __name__ == "__main__":
     with torch.no_grad():
         for entry in validation_loader:
             # Assuming your model outputs segmentation masks
-            outputs = unet( entry[0], entry[1], 
-                           entry[3], entry[4])
+            outputs = unet( entry[0], entry[1])
 
             # Convert logits to binary masks using a threshold or softmax
             pred_masks = (torch.sigmoid(outputs) > 0.5).float()
